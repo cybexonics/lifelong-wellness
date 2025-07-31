c00771cb00a7f0f01dc11b24e2f527c03eade8a6
cybexonics <cybexonicsitconsultants@gmail.com>
2025-07-31 17:39:15 +0530

    form

--- src/components/BookingForm.tsx
+++ src/components/BookingForm.tsx
@@ -40,7 +40,7 @@ const BookingForm = () => {
 
       console.log("Submitting booking form:", submitData)
 
-      const response = await fetch("http://localhost:3001/api/send-email", {
+      const response = await fetch("https://www.lifelongwellness.co.in/api/send-email", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
