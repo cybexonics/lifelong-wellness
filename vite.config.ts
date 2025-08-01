8973ee06003709644bf2de1f4c18af154f5028da
cybexonics <cybexonicsitconsultants@gmail.com>
2025-08-01 15:34:54 +0530

    build

--- vite.config.ts
+++ vite.config.ts
@@ -1,22 +1,27 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
-import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
-export default defineConfig(({ mode }) => ({
-  server: {
-    host: "::",
-    port: 8080,
-  },
-  plugins: [
-    react(),
-    mode === 'development' &&
-    componentTagger(),
-  ].filter(Boolean),
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
+export default defineConfig(async ({ mode }) => {
+  let taggerPlugin: any = undefined;
+  if (mode === "development") {
+    const mod = await import("lovable-tagger");
+    taggerPlugin = mod.componentTagger();
+  }
+  return {
+    server: {
+      host: "::",
+      port: 8080,
     },
-  },
-}));
+    plugins: [
+      react(),
+      taggerPlugin,
+    ].filter(Boolean),
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "./src"),
+      },
+    },
+  };
+});
