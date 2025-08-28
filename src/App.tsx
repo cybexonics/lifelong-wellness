da7c7322f2142cb46531a52087ef5016dde13aa7
cybexonics <cybexonicsitconsultants@gmail.com>
2025-08-29 00:54:36 +0530

    top

--- src/App.tsx
+++ src/App.tsx
@@ -10,6 +10,7 @@ import Services from "./pages/Services"
 import Gallery from "./pages/Gallery"
 import Contact from "./pages/Contact"
 import NotFound from "./pages/NotFound"
+import ScrollToTop from "./components/ScrollToTop" 
 
 const queryClient = new QueryClient()
 
@@ -19,6 +20,7 @@ const App = () => (
       <Toaster />
       <Sonner />
       <BrowserRouter>
+      
         <Routes>
           <Route path="/" element={<Index />} />
           <Route path="/about" element={<About />} />
