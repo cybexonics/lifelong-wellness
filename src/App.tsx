f92caabef7e43a8b59d67f6124fc4fad6d829e3e
cybexonics <cybexonicsitconsultants@gmail.com>
2025-09-10 18:07:37 +0530

    ..

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom"
 import Index from "./pages/Index"
 import About from "./pages/About"
 import Methods from "./pages/Methods" // Make sure this matches the export
-import Services from "./pages/Services";
+import Services from "./pages/Services"
 import Gallery from "./pages/Gallery"
 import Contact from "./pages/Contact"
 import NotFound from "./pages/NotFound"
