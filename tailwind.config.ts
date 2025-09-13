c02d294c13b115d3cc81dab9bbc5a1f2cdd8c395
mr.sanskar19 <mr.sanskar19@gmail.com>
2025-09-13 07:56:05 +0000

    test

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,6 +7,7 @@ const config: Config = {
     "./components/**/*.{ts,tsx}",
     "./app/**/*.{ts,tsx}",
     "./src/**/*.{ts,tsx}",
+    "./index.html",
   ],
   prefix: "",
   theme: {
--- vite.config.ts
+++ vite.config.ts
@@ -4,18 +4,12 @@ import path from "path"
 
 // https://vitejs.dev/config/
 export default defineConfig(async ({ mode }) => {
-  let taggerPlugin: any = undefined
-  if (mode === "development") {
-    const mod = await import("lovable-tagger")
-    taggerPlugin = mod.componentTagger()
-  }
-
   return {
     server: {
       host: "::",
       port: 8080,
     },
-    plugins: [react(), taggerPlugin].filter(Boolean),
+    plugins: [react()],
     resolve: {
       alias: {
         "@": path.resolve(__dirname, "./src"),
